d522e19f4b2be11d0aefaf45c81e661fd41a19b5
Lam Yuen Ho Joseph <yuenholam@gmail.com>
2024-04-08 23:44:31 +0800

    Fix email link in Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
   return (
     <section className='section contact center' id='contact'>
       <h2 className='section__title'>Contact</h2>
-      <a href={`mailto:${contact.email}`}>
+      <a href={`mailto:${contact.email}`} aria-label='email'>
         <span type='button' className='btn btn--outline'>
           Email me
         </span>
