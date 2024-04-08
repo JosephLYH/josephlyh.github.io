ba782aadafd5441f9fd825883e47fb3ead92df76
Lam Yuen Ho Joseph <yuenholam@gmail.com>
2024-04-08 23:42:20 +0800

    Fix resume link in About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -19,7 +19,7 @@ const About = () => {
 
       <div className='about__contact center'>
         {resume && (
-          <a href={resume}>
+          <a href={resume} aria-label='resume'>
             <span type='button' className='btn btn--outline'>
               Resume
             </span>
