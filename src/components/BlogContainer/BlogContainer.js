127a9f42634e6038d9947600f43b724835b2b4ec
Lam Yuen Ho Joseph <yuenholam@gmail.com>
2024-04-11 00:58:21 +0800

    Refactor blog post component styles and structure

--- src/components/BlogContainer/BlogContainer.js
+++ src/components/BlogContainer/BlogContainer.js
@@ -2,13 +2,15 @@ import './BlogContainer.css'
 
 const BlogContainer = ({ post }) => (
   <div className='post'>
-    <p className='timestamp'>{new Date(post.timestamp).toLocaleString()}</p>
+    <p className='post__timestamp'>
+      {new Date(post.timestamp).toLocaleString()}
+    </p>
 
     <h3>{post.title}</h3>
 
-    <p className='post__stack-item'>{post.description}</p>
+    <p className='post__description'>{post.description}</p>
 
-    <p className='post__description'>{post.content}</p>
+    <p className='post__content'>{post.content}</p>
   </div>
 )
 
