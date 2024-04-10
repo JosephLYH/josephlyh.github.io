71d2055be6980ea98e5b1c864488216d3d6ab07f
Lam Yuen Ho Joseph <yuenholam@gmail.com>
2024-04-10 23:29:38 +0800

    add blog post sample

---
+++ src/blogPosts.js
@@ -0,0 +1,11 @@
+const posts = [
+  {
+    id: 0,
+    title: 'Hello World!',
+    timestamp: '1712762673839',
+    description: 'This is my first blog post!',
+    content: 'This is my first blog post! I will be sharing my thoughts and experiences here. Stay tuned!'
+  }
+]
+
+export default posts
